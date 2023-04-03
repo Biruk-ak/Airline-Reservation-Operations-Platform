9e5b4d294f424a92e5cc69f397caa45ff835ba28
Biruk-ak <birukaklilu0110@gmail.com>
2023-04-03 13:55:00 +0300

    Build React consoles for scheduling and passenger operations
    Add module pages, API clients, and utility helpers for flight scheduling,
    aircraft, crew, bookings, seats, check-in, boarding, gates, baggage, and cargo
    including list filters, bulk actions, detail drawers, and export controls.

---
+++ frontend/src/modules/Boarding/BoardingApi.js
@@ -0,0 +1,22 @@
+import api from '../../services/api';
+
+const BASE = '/boarding';
+
+export const BoardingApi = {
+  list: (params) => api.get(BASE, { params }),
+  get: (id) => api.get(`${BASE}/${id}`),
+  create: (payload) => api.post(BASE, payload),
+  update: (id, payload) => api.put(`${BASE}/${id}`, payload),
+  remove: (id) => api.delete(`${BASE}/${id}`),
+  activate: (id) => api.post(`${BASE}/${id}/activate`),
+  deactivate: (id, reason) => api.post(`${BASE}/${id}/deactivate`, { reason }),
+  archive: (id, reason) => api.post(`${BASE}/${id}/archive`, { reason }),
+  bulk: (action, ids, payload = {}) => api.post(`${BASE}/bulk`, { action, ids, payload }),
+  statistics: () => api.get(`${BASE}/statistics`),
+  export: (params) => api.get(`${BASE}/export`, { params }),
+  timeline: (id) => api.get(`${BASE}/${id}/timeline`),
+  clone: (id, overrides = {}) => api.post(`${BASE}/${id}/clone`, { overrides }),
+  syncExternal: (id, payload) => api.post(`${BASE}/${id}/sync-external`, payload),
+};
+
+export default BoardingApi;
